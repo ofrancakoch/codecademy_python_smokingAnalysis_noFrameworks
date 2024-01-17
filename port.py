8d09d2f83eebba945fad7fcc3b52cb2dcb95deed
Octávio França Koch <145387146+ofrancakoch@users.noreply.github.com>
2024-01-17 11:55:03 -0300

    Update port.py

--- port.py
+++ port.py
@@ -2,20 +2,20 @@ class Insurance():
     def __init__(self):
         self.arquivo = 'insurance.csv'
 
-    def OpenFile(self, index): #To open file 'insurance.csv and separate it by commas'
+    def openFile(self, index): #To open file 'insurance.csv and separate it by commas'
         with open(self.arquivo) as csvfile:
             lst = [line.split(',')[index] for line in csvfile]
         csvfile.close()
         return lst
 
-    def UniqueValues(self, index):#To find unique values ​​in certain inserted columns
+    def uniqueValues(self, index):#To find unique values ​​in certain inserted columns
         lst = list()
         for element in self.OpenFile(index):
             if element not in lst:
                 lst.append(element)
         return lst
 
-    def ValueCounting(self, index):#To count occurrences of unique values
+    def valueCounting(self, index):#To count occurrences of unique values
         dicio = {}   
         for element in self.OpenFile(index):
             if element in dicio:
@@ -24,14 +24,14 @@ class Insurance():
                 dicio[element] = 1
         return sorted(dicio.items(), key=lambda x:x[1], reverse=True)
 
-    def Filter(self, dicio: dict, index): #To choose by certain inserted conditions
+    def filter(self, dicio: dict, index): #To choose by certain inserted conditions
         dio = {}
         for word, value in dicio.items():
             if word in self.UniqueValues(index):
                 dio[word] = value
         return sorted(dio.items(), key=lambda x:x[1], reverse=True)
 
-    def SmokingAnalysis(self, afirmneg: str): #Final Analysis (No smokers and smokers).
+    def smokingAnalysis(self, afirmneg: str): #Final Analysis (No smokers and smokers).
         dicio = {}
         with open(self.arquivo) as csvfile:
             lst = [line.split(',')for line in csvfile]
@@ -56,7 +56,7 @@ class Insurance():
             print(f"There are more people without children that smoke.\nProof:\n{self.Filter(dicio,3)}\n")
     
 analysis = Insurance()
-analysis.SmokingAnalysis('yes')
-analysis.SmokingAnalysis('no')
-print(analysis.UniqueValues(1))
-print(analysis.ValueCounting(4))
+analysis.smokingAnalysis('yes')
+analysis.smokingAnalysis('no')
+print(analysis.uniqueValues(1))
+print(analysis.valueCounting(4))
